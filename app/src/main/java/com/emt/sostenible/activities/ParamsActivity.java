f5b704b271415921e602d26001dc034ede488858
Israel <israel.concoma@gmail.com>
2019-03-30 18:28:50 +0100

    Initial Commit 2

---
+++ app/src/main/java/com/emt/sostenible/activities/ParamsActivity.java
@@ -0,0 +1,21 @@
+package com.emt.sostenible.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.emt.sostenible.R;
+
+public class ParamsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_params);
+
+    }
+
+}
