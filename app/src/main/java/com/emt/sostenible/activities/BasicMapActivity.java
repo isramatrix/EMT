c2eda4e561f8dd5541aca37c75e9764d572a7c21
Israel <israel.concoma@gmail.com>
2019-03-30 18:35:43 +0100

    Muestra cosas

--- app/src/main/java/com/emt/sostenible/activities/BasicMapActivity.java
+++ app/src/main/java/com/emt/sostenible/activities/BasicMapActivity.java
@@ -2,12 +2,16 @@ package com.emt.sostenible.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.emt.sostenible.R;
 import com.here.android.mpa.common.GeoCoordinate;
 import com.here.android.mpa.common.OnEngineInitListener;
 import com.here.android.mpa.mapping.Map;
 import com.here.android.mpa.mapping.MapFragment;
+import com.here.android.mpa.mapping.SupportMapFragment;
+
+import java.io.File;
 
 public class BasicMapActivity extends Activity {
 
@@ -28,25 +32,31 @@ public class BasicMapActivity extends Activity {
 
         // Search for the map fragment to finish setup by calling init().
         mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapfragment);
-
-        mapFragment.init(new OnEngineInitListener() {
-            @Override
-            public void onEngineInitializationCompleted(OnEngineInitListener.Error error) {
-                if (error == OnEngineInitListener.Error.NONE) {
-                    // retrieve a reference of the map from the map fragment.
-                    map = mapFragment.getMap();
-
-                    // Set the map center to the Vancouver region (no animation).
-                    map.setCenter(new GeoCoordinate(49.196261, -123.004773, 0.0),
-                            Map.Animation.NONE);
-
-                    // Set the zoom level to the average between min and max.
-                    map.setZoomLevel((map.getMaxZoomLevel() + map.getMinZoomLevel()) / 2);
-
-                } else {
-                    System.out.println("ERROR: Cannot initialize Map Fragment");
+        // Set up disk cache path for the map service for this application
+        // It is recommended to use a path under your application folder for storing the disk cache
+        boolean success = com.here.android.mpa.common.MapSettings.setIsolatedDiskCacheRootPath(
+                getApplicationContext().getExternalFilesDir(null) + File.separator + ".here-maps",
+                "EMTService"); /* ATTENTION! Do not forget to update {YOUR_INTENT_NAME} */
+        if (!success) {
+            Toast.makeText(getApplicationContext(), "Unable to set isolated disk cache path.",
+                    Toast.LENGTH_LONG);
+        } else {
+            mapFragment.init(new OnEngineInitListener() {
+                @Override
+                public void onEngineInitializationCompleted(OnEngineInitListener.Error error) {
+                    if (error == OnEngineInitListener.Error.NONE) {
+                        // retrieve a reference of the map from the map fragment
+                        map = mapFragment.getMap();
+                        // Set the map center to the Vancouver region (no animation)
+                        map.setCenter(new GeoCoordinate(49.196261, -123.004773, 0.0),
+                                Map.Animation.NONE);
+                        // Set the zoom level to the average between min and max
+                        map.setZoomLevel((map.getMaxZoomLevel() + map.getMinZoomLevel()) / 2);
+                    } else {
+                        System.out.println("ERROR: Cannot initialize Map Fragment");
+                    }
                 }
-            }
-        });
+            });
+        }
     }
 }
