53e8e5c3224c8250d3c9a21f58b2b7d9e39e0fbc
willmontaraces <pabance@inf.upv.es>
2019-04-01 12:32:52 +0200

    Gato con botas

--- app/src/main/java/com/emt/sostenible/data/DataFetcher.java
+++ app/src/main/java/com/emt/sostenible/data/DataFetcher.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.util.ArrayMap;
 
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -13,22 +14,33 @@ import java.util.Scanner;
 import org.json.*;
 
 
+
 public class DataFetcher {
 
     //Mapa de paradas indexado por enteros que representan el numero de linea
     private static Map<Integer, ArrayList<Parada>> stopsMap;
     //Lista de paradas
-    private static Parada[] paradas;
+    private static Parada[] listaParadas;
+    private static JSONObject paradas;
 
     public DataFetcher(final Context context)
     {
         try {
             InputStream is = context.getAssets().open("json/localizacionParadas.json");
-
-
+            StringBuilder stBuild = new StringBuilder();
+            Scanner sc = new Scanner(is);
+            while(sc.hasNextLine()){
+                stBuild.append(sc.nextLine());
+            }
+            sc.close();
+            is.close();
+            String result = stBuild.toString();
+            paradas = new JSONObject(result);
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+
     }
 
     public Map<Line, Parada> getAllStops()
